ed9732b87bec120649a1302c9c9f7e92392551c0
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2020-08-17 17:19:29 +0430

    Updates text client.

--- text_client.py
+++ text_client.py
@@ -4,10 +4,9 @@ import logging
 import json
 import struct
 from dataclasses import dataclass, asdict
-from gtts import gTTS 
-import pygame
+# from gtts import gTTS
+# import pygame
 language = "en"
-
 
 LOG_FORMAT = "%(asctime)s - [%(levelname)s] %(message)s"
 logging.basicConfig(format=LOG_FORMAT)
@@ -16,7 +15,7 @@ logging.getLogger().setLevel(logging.INFO)
 from socket import socket, AF_INET, SOCK_STREAM
 logger = logging.getLogger(__name__)
 
-HOST_ADDRESS = "192.168.1.75"
+HOST_ADDRESS = "192.168.11.128"
 PORT = 8080
 
 @dataclass
@@ -29,11 +28,28 @@ class Request:
           return json.dumps(asdict(self))
 
 def cleanup_json(json: str) -> str:
-     escape_characters = ["\n", "\r", "\t", "\x00"]
-     for e in escape_characters:
-          json = json.strip(e)
+     replace_characters = {
+          "\n": "",
+          "\r": "",
+          "\t": "",
+          "\x00": "",
+          "{'": '{\"',
+          "'}": '\"}',
+          "':": '\":',
+          ":'": ':\"',
+          "',": '\",',
+          ",'": ',\"'
+     }
+
+     for k, v in replace_characters.items():
+          json = json.replace(k, v)
 
      return json
+     # escape_characters = ["\n", "\r", "\t", "\x00"]
+     # for e in escape_characters:
+     #      json = json.strip(e)
+
+     # return json
 
 class Client:
      def __init__(self, host: str, port: int) -> None:
@@ -44,29 +60,30 @@ class Client:
           self.sock.connect((HOST_ADDRESS, PORT))
 
           self.send_auth_msg()
-          
-          pygame.mixer.init()
+
+          # pygame.mixer.init()
 
      def send_auth_msg(self) -> None:
           req = Request("Python", "me", "aut")
           self.send_request(req)
-    
+
      def send_user_input(self, user_input: str) -> None:
           logger.info(f"Send user input: {user_input}")
           req = Request(user_input, "me", "txt")
           self.send_request(req)
           res = self.read_response()
-          res_parsed = json.loads(res)
-          reply = res_parsed['m'].replace("'", ' " ')
-          reply_parsed = json.loads(reply)
-          print(reply['type'])       
-     #     New function needed 
+          print(res)
+          # res_parsed = json.loads(res)
+          # reply = res_parsed['m'].replace("'", ' " ')
+          # reply_parsed = json.loads(reply)
+          # print(reply['type'])
+     #     New function needed
      #     speech = gTTS(text = res_parsed['m'], lang = language, slow = False)
      #     speech.save("text.mp3")
      #     pygame.mixer.music.load("text.mp3")
      #     pygame.mixer.music.play()
 
-          
+
      def send_request(self, req: Request) -> None:
           msg_json = req.to_json().replace('"', '\"')
           msg_len = len(msg_json)
@@ -86,12 +103,12 @@ class Client:
 
           return cleanup_json(msg.decode("UTF-8").strip())
 
+if __name__ == "__main__":
+     client = Client(HOST_ADDRESS, PORT)
 
-client = Client(HOST_ADDRESS, PORT)
+     while True:
+          user_input = input("Enter a text: ")
+          client.send_user_input(user_input)
 
-while True:
-     user_input = input("Enter a text: ")
-     client.send_user_input(user_input)
-     
-     while pygame.mixer.music.get_busy() == True: 
-          continue
+          #while pygame.mixer.music.get_busy() == True:
+          #     continue
