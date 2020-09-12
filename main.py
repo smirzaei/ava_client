214d2f74910b3a5a973ff808ff5a74a87396b4b3
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2020-09-12 12:29:00 +0430

    Running listener on a different thread to avoid blocking LEDs.

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ from alexa_led_pattern import AlexaLedPattern
 from google_home_led_pattern import GoogleHomeLedPattern
 import speech_recognition as sr
 from ava import Ava
+from concurrent.futures import ThreadPoolExecutor
 import wave
 
 # from vr import get_user_voice_input
@@ -32,7 +33,7 @@ import wave
 HOST_ADDRESS = "192.168.11.142"
 PORT = 8080
 
-# client = Client(HOST_ADDRESS, PORT)
+client = Client(HOST_ADDRESS, PORT)
 
 logger = logging.getLogger(__name__)
 ava = Ava()
@@ -55,56 +56,38 @@ def main():
         pixels.wakeup(direction)
         time.sleep(2)
         pixels.listen()
-        listen_result = ava.listen()
 
-        audio_data = sr.AudioData(listen_result, 16000, 1)
+        with ThreadPoolExecutor(1) as executor:
+            future = executor.submit(ava.listen)
+            result = future.result()
+            print(result)
 
-        # r.adjust_for_ambient_noise()
-        # result = r.recognize_google(audio_data, language="en-US")
-        # logger.info(f"Google result: {result}")
+            listen_result = ava.listen()
 
-        file_path = "/tmp/ava_tmp.wav"
-        logger.info(f"Writing audio data to temp file. {file_path}")
-        with wave.open(file_path, mode="wb") as file:
-            file.setnchannels(1)
-            file.setsampwidth(2)
-            file.setframerate(16000)
-            file.writeframesraw(listen_result)
+            file_path = "/tmp/ava_tmp.wav"
+            logger.info(f"Writing audio data to temp file. {file_path}")
+            with wave.open(file_path, mode="wb") as file:
+                file.setnchannels(1)
+                file.setsampwidth(2)
+                file.setframerate(16000)
+                file.writeframesraw(listen_result)
 
-        logger.info(f"Reading audio data from temp file {file_path}")
-        with sr.AudioFile(file_path) as file:
-            audio = r.record(file)
+            logger.info(f"Reading audio data from temp file {file_path}")
+            with sr.AudioFile(file_path) as file:
+                audio = r.record(file)
 
-            try:
-                logger.info("Sending audio data to google for recognition.")
-                recog = r.recognize_google(audio, language = 'en-US')
+                try:
+                    logger.info("Sending audio data to google for recognition.")
+                    recog = r.recognize_google(audio, language = 'en-US')
 
-                logger.info("You said: " + recog)
-                # client.send_user_input(user_input)
-            except sr.UnknownValueError:
-                logger.error("Google Speech Recognition could not understand audio")
-            except sr.RequestError as e:
-                logger.error("Could not request results from Google Speech Recognition service; {0}".format(e))
+                    logger.info("You said: " + recog)
+                    client.send_user_input(recog)
+                except sr.UnknownValueError:
+                    logger.error("Google Speech Recognition could not understand audio")
+                except sr.RequestError as e:
+                    logger.error("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-
-        # logger.info("Adjusting for ambient noise")
-        # audio = r.adjust_for_ambient_noise(src)
-        # logger.info("Listening...")
-        # audio = r.listen(src)
-
-        # try:
-        #     recog = r.recognize_google(audio, language = 'en-US')
-
-        #     logger.info("You said: " + recog)
-        # except sr.UnknownValueError:
-        #     logger.error("Google Speech Recognition could not understand audio")
-        # except sr.RequestError as e:
-        #     logger.error("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-#       user_input = get_user_voice_input()
-#        client.send_user_input(user_input)
-
-        pixels.off()
+            pixels.off()
 
 
     kws.on_detected = on_detected
